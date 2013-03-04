65caf90b09c480a2702e6b6f4492de57ed92dc2f
Dan Kogai <dankogai+github@gmail.com>
2013-03-04 20:42:19 +0900

    VERSION 0.01

--- cocytus.js
+++ cocytus.js
@@ -1,5 +1,5 @@
 /*
- * $Id$
+ * $Id: cocytus.js,v 0.1 2013/03/04 11:41:48 dankogai Exp dankogai $
  *
  *  Licensed under the MIT license.
  *  http://www.opensource.org/licenses/mit-license.php
@@ -8,60 +8,62 @@
  *    https://developer.mozilla.org/en-US/docs/DOM/Using_web_workers
  *    http://ejohn.org/blog/ecmascript-5-objects-and-properties/
  */
-
-(function(global) {
-    if (! global.document ) { // I'm a web worker
+ 
+(function (global) {
+    if (!global.document) { // I'm a web worker
         var myFunction = global.Function;
-        global.onmessage = function(event) {
+        global.onmessage = function (event) {
             'use strict';
             var src = event.data;
             if (src.match(/^function/)) src = src.replace(/^[^\{]+/, '');
-            try{
+            try {
                 (new myFunction(src))();
-            }catch(e){
+            } catch (e) {
                 throw e;
             }
         };
-        Object.getOwnPropertyNames(global).forEach(function(p) {
-                var go = global[p];
-                if (!go) return;                      // undefined | null
-                if (Object(go) !== go) return;        // non-objects
-                if (go.hasOwnProperty('prototype')) { // has no prototype
-                    // console.log(p);
-                    var proto = go.prototype;
-                    if (!proto.hasOwnProperty('constructor')) return;
-                    // console.log(p + '.' + 'constructor');
-                    delete proto.constructor;
-                    Object.freeze(proto);
-                }
-            });
+        Object.getOwnPropertyNames(global).forEach(function (p) {
+            var go = global[p];
+            if (!go) return;                        // undefined | null
+            if (Object(go) !== go) return;          // non-objects
+            if (go.hasOwnProperty('prototype')) {   // has no prototype
+                var proto = go.prototype;
+                if (!proto.hasOwnProperty('constructor')) return;
+                delete proto.constructor;
+                Object.freeze(proto);
+            }
+        });
         delete global.Function;
         delete global.Worker;
         delete global.SharedWorker;
         delete global.eval;
-        if (global.uneval) Object.seal(global);   // Firefox
-        else               Object.freeze(global); // anything else
-    } else {                // I'm a browser document
-        if (! global.Worker ) throw 'Web Worker unsupported';
+        if (global.uneval) Object.seal(global);     // Firefox
+        else               Object.freeze(global);   // anything else
+    } else { // I'm a browser document
+        if (!global.Worker) throw 'Web Worker unsupported';
         var scripts = document.getElementsByTagName('script'),
-            me      = scripts[scripts.length - 1],
-            myuri   = me.src;
-        var cocytus = function(o) {
+            me = scripts[scripts.length - 1],
+            myuri = me.src;
+        var cocytus = function (o) {
             if (!(this instanceof cocytus)) return new cocytus(o);
             var worker = new Worker(myuri);
             // defaults
-            worker.onmessage = function(ev) { console.log(ev) };
-            worker.onerror = function(ev) { 
+            worker.onmessage = function (ev) {
+                console.log(ev)
+            };
+            worker.onerror = function (ev) {
                 console.log(ev);
                 this.terminate();
             };
             worker.timeout = 1000; // ms
-            worker.run = function(work, timeout) { 
+            worker.run = function (work, timeout) {
                 this.postMessage('' + work);
                 if (!timeout) timeout = 0 + this.timeout;
                 if (timeout) {
                     var that = this;
-                    setTimeout(function(){ that.terminate() }, timeout);
+                    setTimeout(function () {
+                        that.terminate()
+                    }, timeout);
                 }
             };
             if (Object(o) === o) for (var p in o) worker[p] = o[p];
